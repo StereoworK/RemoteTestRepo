ed2bb46c3312249a894e555bdd23f952399791ff
SivkovAV <stereoworkmail@gmail.com>
2022-02-20 18:50:43 +0300

    add MyClass

--- main.scala
+++ main.scala
@@ -1,5 +1,11 @@
+class MyClass{
+  def print(): Unit = println("This is MyClass method")
+}
+
 object MyApp{
   def Main(args: Array[String]): Unit = {
       println("this is main function")
+      val myClass = new MyClass()
+      myClass.print()
   }
 }
